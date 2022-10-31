7ff22ed57a470e7a99ef7f1e3682e265d7224616
drew-post <drew_post@outlook.com>
2022-10-31 14:05:32 -0500

    Initial Commit

---
+++ src/components/DealDetails.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {priceDisplay} from '../util';
+import ajax from '../ajax';
+
+class DealDetails extends React.Component {
+  static propTypes = {
+    initialDealData: PropTypes.object.isRequired,
+  };
+  state = {
+    deal: this.props.initialDealData,
+  };
+  async componentDidMount() {
+    const fullDeal = await ajax.fetchDealDetail(this.state.deal.key);
+    this.setState({
+      deal: fullDeal,
+    });
+  }
+  render() {
+    const {deal} = this.state;
+    return (
+      <View style={styles.deal}>
+        <Image source={{uri: deal.media[0]}} style={styles.image} />
+        <View style={styles.detail}>
+          <View>
+            <Text style={styles.title}>{deal.title}</Text>
+          </View>
+          <View style={styles.footer}>
+            <View style={styles.info}>
+              <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+              <Text style={styles.cause}>{deal.cause.name}</Text>
+            </View>
+            {deal.user && (
+              <View style={styles.user}>
+                <Image source={{uri: deal.user.avatar}} style={styles.avatar} />
+                <Text>{deal.user.name}</Text>
+              </View>
+            )}
+          </View>
+          <View style={styles.description}>
+            <Text>{deal.description}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  deal: {
+    marginHorizontal: 12,
+    marginTop: 60,
+    borderColor: '#bbb',
+    borderWidth: 1,
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    backgroundColor: '#ccc',
+  },
+  detail: {},
+  info: {
+    alignItems: 'center',
+  },
+  user: {
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 16,
+    padding: 10,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(237, 149,45, 0.4)',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  cause: {
+    alignItems: 'center',
+  },
+  price: {
+    fontWeight: 'bold',
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  description: {
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderStyle: 'dotted',
+    margin: 10,
+    padding: 10,
+  },
+});
+
+export default DealDetails;
